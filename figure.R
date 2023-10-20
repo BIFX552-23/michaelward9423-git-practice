2e93ace159fcc2a71c27cfbc4f4de6972bbddf4f
michaelward9423 <mdw5@hood.edu>
2023-10-19 20:46:42 -0400

    now with figure

--- figure.R
+++ figure.R
@@ -1,11 +1,16 @@
 # a figure
 
 library(dplyr)
+
 library(ggplot2)
 library(cowplot)
 theme_set(theme_cowplot())
 
-set.seed(462821)
-dat <- tibble(x = rnorm(100), y = x + rnorm(100, sd = 0.2))
+set.seed(298347)
+dat <- tibble(x = rnorm(100), 
+              y = x + rnorm(100, sd = 0.2))
 
-ggplot(dat, aes(x,y)) + geom_point()
+g1 <- ggplot(dat, aes(x, y)) +
+    geom_point()
+
+ggsave('figure.png', g1)
