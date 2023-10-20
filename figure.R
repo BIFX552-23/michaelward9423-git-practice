8369cd1bf7391fec3a0c3defc445c66d5291b3d6
michaelward9423 <143763521+michaelward9423@users.noreply.github.com>
2023-10-19 20:00:53 -0400

    figure added

---
+++ figure.R
@@ -0,0 +1,11 @@
+# a figure
+
+library(dplyr)
+library(ggplot2)
+library(cowplot)
+theme_set(theme_cowplot())
+
+set.seed(462821)
+dat <- tibble(x = rnorm(100), y = x + rnorm(100, sd = 0.2))
+
+ggplot(dat, aes(x,y)) + geom_point()
